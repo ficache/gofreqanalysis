2dd06c981fa9736b6e372e658e32bc2c3ac5743a
ficache <dixxe.mail@proton.me>
2024-11-15 21:48:34 +0300

    Add flake remove binaries

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A portable go dev flake!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: 
+	let
+    system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+  in 
+	{
+    devShells.${system} = {
+		  default = pkgs.mkShell {
+			  nativeBuildInputs = with pkgs; [
+				  go
+					gopls
+				];
+
+				shellHook = "echo Welcome to go environment";
+			};
+		};
+	};
+}
